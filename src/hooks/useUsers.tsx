83ad64b3eb7cfa06be4f17eeee35e477500e5808
silvina_munoz <silvina.munoz@neutroon.com>
2024-07-04 16:02:25 +0200

    Updating custom hook to return pagination object

--- src/hooks/useUsers.tsx
+++ src/hooks/useUsers.tsx
@@ -7,15 +7,40 @@ export interface User {
     email: string
     avatar: string
 }
-const useUsers = () => {
-    const { data: users = [], isLoading } = useQuery<User[]>({
-        queryKey: ['users'],
-        queryFn: () => fetchUsers(1),
-        placeholderData: keepPreviousData,
+
+/**
+ * Represents the query response containing a list of users, pagination information,
+ * and total number of pages.
+ */
+export interface UsersQuery {
+    users: User[];
+    currentPage: number;
+    perPage: number;
+    totalPages: number;
+}
+
+/**
+ * Custom hook to fetch users data with pagination support.
+ * @param page The current page number.
+ * @param itemsPerPage Number of items per page.
+ * @returns Object containing users data, pagination details, loading state, fetch status, and refetch function.
+ */
+const useUsers = (page?: number, itemsPerPage?: number) => {
+    const { data: usersQuery, isLoading, isFetching, refetch } = useQuery<UsersQuery, Error, UsersQuery>({
+        queryKey: ['users', page, itemsPerPage],
+        queryFn: () => fetchUsers(page, itemsPerPage),
+        placeholderData: keepPreviousData, //to maintain previous data while fetching new data.
     })
     return {
-        users,
-        isLoading
+        users: usersQuery?.users,
+        pagination: {
+            currentPage: usersQuery?.currentPage,
+            perPage: usersQuery?.perPage,
+            totalPages: usersQuery?.totalPages
+        },
+        isLoading,
+        isFetching,
+        refetch
     }
 }
 
