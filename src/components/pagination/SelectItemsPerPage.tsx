e06949e3bd4433172e9ab7140c48f81eb771870f
silvina_munoz <silvina.munoz@neutroon.com>
2024-07-04 16:03:43 +0200

    Adding table and items per page component

---
+++ src/components/pagination/SelectItemsPerPage.tsx
@@ -0,0 +1,34 @@
+import { Label } from "flowbite-react";
+import { ChangeEvent } from "react";
+
+interface SelectItemsProps {
+    itemsPerPage: number;
+    setItemsPerPage: (item: number) => void
+}
+
+const SelectItemsPerPage = (props: SelectItemsProps) => {
+    const { itemsPerPage, setItemsPerPage } = props;
+    const items = [1, 2, 5, 10];
+    const onChangeHandler = (event: ChangeEvent<HTMLSelectElement>) => {
+        const value = event.target.value;
+        setItemsPerPage(Number(value));
+    };
+    return (
+        <div className="flex items-center gap-3 ">
+            <Label className="text-sm text-slate-500">Items per page</Label>
+            <select
+                id="items-per-page"
+                value={itemsPerPage}
+                onChange={onChangeHandler}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            >
+                <option>Select</option>
+                {items.map((item) => (
+                    <option key={item} value={item}>{item}</option>
+                ))}
+            </select>
+        </div>
+    )
+}
+
+export default SelectItemsPerPage;
---
+++ src/components/table/Table.tsx
@@ -0,0 +1,53 @@
+
+import { Table } from "flowbite-react";
+import { User } from "../../hooks/useUsers";
+import Pagination from "../pagination/Pagination";
+interface TableCompProps {
+    data: User[] | undefined;
+    currentPage: number;
+    itemsPerPage: number;
+    totalPages: number;
+    setCurrentPage: (page: number) => void;
+    setItemsPerPage: (items: number) => void;
+
+}
+const TableComponent = (props: TableCompProps) => {
+    const { data: users, currentPage, itemsPerPage, setCurrentPage, setItemsPerPage, totalPages } = props;
+    return (
+        <>
+            <Table className="relative">
+                <Table.Head className="text-left">
+                    <Table.HeadCell>ID</Table.HeadCell>
+                    <Table.HeadCell>Name</Table.HeadCell>
+                    <Table.HeadCell>Email</Table.HeadCell>
+                </Table.Head>
+                <Table.Body className="divide-y">
+                    {users?.length === 0 ? (
+                        <p>No Results</p>
+                    ) : (users?.map((user: User) => {
+                        return <Table.Row key={user?.id} className="bg-white py-4">
+                            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                                {user?.id}
+                            </Table.Cell>
+                            <Table.Cell className="flex items-center gap-2">
+                                <img className="rounded-full w-10" src={user?.avatar} />
+                                <div> {`${user?.first_name} ${user?.last_name}`}</div>
+                            </Table.Cell>
+                            <Table.Cell>{user?.email}</Table.Cell>
+                        </Table.Row>
+                    }))
+                    }
+                </Table.Body>
+            </Table>
+            <Pagination
+                currentPage={currentPage}
+                itemsPerPage={itemsPerPage}
+                setCurrentPage={setCurrentPage}
+                setItemsPerPage={setItemsPerPage}
+                totalPages={totalPages}
+            />
+        </>
+    )
+}
+
+export default TableComponent;
