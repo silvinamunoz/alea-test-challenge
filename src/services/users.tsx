024d4098031078a4f6baa37785ab6bfe3a2ba642
silvina_munoz <silvina.munoz@neutroon.com>
2024-07-04 16:01:24 +0200

    Updating users return fetched data with pagination

--- src/services/users.tsx
+++ src/services/users.tsx
@@ -1,15 +1,15 @@
-export const fetchUsers = async (page: number) => {
-    console.log(page);
-
-    return await fetch(`https://reqres.in/api/users?page=${page}`)
+export const fetchUsers = async (page?: number, itemsPerPage?: number) => {
+    return await fetch(`https://reqres.in/api/users?page=${page}&per_page=${itemsPerPage}`)
         .then(async res => {
             if (!res.ok) throw new Error('Something went wrong with the users');
             return await res.json();
         })
         .then(res => {
-            return res.data
-        })
-        .catch(err => {
-            console.log(err);
+            return {
+                users: res.data,
+                currentPage: res.page,
+                perPage: res.per_page,
+                totalPages: res.total_pages
+            }
         })
 }
